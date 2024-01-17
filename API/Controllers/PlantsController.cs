e627bd6d1dfdfa26b37a1b9f6c9547d44b7534b6
Migle Urbonaite <91160873+MistyDoe@users.noreply.github.com>
2024-01-17 22:04:49 +0100

    API: fixed Plant controller

--- API/Controllers/PlantsController.cs
+++ API/Controllers/PlantsController.cs
@@ -1,124 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using API.Data;
+using API.DTOs;
+using API.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using API.Data;
-using API.Models;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PlantsController : ControllerBase
-    {
-        private readonly LabManagerDBContext _context;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PlantsController : ControllerBase
+	{
+		private readonly LabManagerDBContext _context;
+		private readonly IMapper _mapper;
 
-        public PlantsController(LabManagerDBContext context)
-        {
-            _context = context;
-        }
+		public PlantsController(LabManagerDBContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
 
-        // GET: api/Plants
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Plant>>> GetPlants()
-        {
-          if (_context.Plants == null)
-          {
-              return NotFound();
-          }
-            return await _context.Plants.ToListAsync();
-        }
+		// GET: api/Plants
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Plant>>> GetPlants()
+		{
+			if (_context.Plants == null)
+			{
+				return NotFound();
+			}
+			var plants = await _context.Plants.Include(p => p.Protocols).ToListAsync();
+			return Ok(plants);
+		}
 
-        // GET: api/Plants/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Plant>> GetPlant(int id)
-        {
-          if (_context.Plants == null)
-          {
-              return NotFound();
-          }
-            var plant = await _context.Plants.FindAsync(id);
+		// GET: api/Plants/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Plant>> GetPlant(int id)
+		{
+			if (_context.Plants == null)
+			{
+				return NotFound();
+			}
+			var plant = await _context.Plants.Include(p => p.Protocols).FirstOrDefaultAsync(pl => pl.Id == id);
 
-            if (plant == null)
-            {
-                return NotFound();
-            }
+			if (plant == null)
+			{
+				return NotFound();
+			}
+			return Ok(plant);
+		}
 
-            return plant;
-        }
+		// PUT: api/Plants/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutPlant(PlantDTO plantDTO)
+		{
+			Plant updatedPlant = new Plant();
 
-        // PUT: api/Plants/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPlant(int id, Plant plant)
-        {
-            if (id != plant.Id)
-            {
-                return BadRequest();
-            }
+			updatedPlant = _mapper.Map<Plant>(plantDTO);
+			updatedPlant.TotalQt = plantDTO.InTSQt + plantDTO.MotherPlantsQt + plantDTO.ForSaleQt;
+			_context.Update(updatedPlant);
+			await _context.SaveChangesAsync();
+			return NoContent();
+		}
+		// POST: api/Plants
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<Plant>> PostPlant(PlantDTO plantDTO)
+		{
+			if (_context.Plants == null)
+			{
+				return Problem("Entity set 'LabManagerDBContext.Plants'  is null.");
+			}
+			var plant = _mapper.Map<Plant>(plantDTO);
+			plant.TotalQt = plantDTO.InTSQt + plantDTO.MotherPlantsQt;
 
-            _context.Entry(plant).State = EntityState.Modified;
+			await _context.Plants.AddAsync(plant);
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PlantExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+			return Created($"api/LabManager/plant/{plant.Id}", plant);
+		}
 
-        // POST: api/Plants
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Plant>> PostPlant(Plant plant)
-        {
-          if (_context.Plants == null)
-          {
-              return Problem("Entity set 'LabManagerDBContext.Plants'  is null.");
-          }
-            _context.Plants.Add(plant);
-            await _context.SaveChangesAsync();
+		// DELETE: api/Plants/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeletePlant(int id)
+		{
+			if (_context.Plants == null)
+			{
+				return NotFound();
+			}
+			var plant = await _context.Plants.FirstOrDefaultAsync(pl => pl.Id == id);
 
-            return CreatedAtAction("GetPlant", new { id = plant.Id }, plant);
-        }
+			_context.Plants.Remove(plant);
+			await _context.SaveChangesAsync();
 
-        // DELETE: api/Plants/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePlant(int id)
-        {
-            if (_context.Plants == null)
-            {
-                return NotFound();
-            }
-            var plant = await _context.Plants.FindAsync(id);
-            if (plant == null)
-            {
-                return NotFound();
-            }
+			return NoContent();
 
-            _context.Plants.Remove(plant);
-            await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
-
-        private bool PlantExists(int id)
-        {
-            return (_context.Plants?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-    }
+		}
+	}
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
 using API.Data;
+using API.DTOs;
+using API.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +19,6 @@ builder.Services.Configure<JsonOptions>(options =>
 	options.SerializerOptions.ReferenceHandler = null;
 	options.SerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
 });
-builder.Services.AddControllers();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
@@ -30,7 +32,182 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-app.MapControllers();
+//app.UseHttpsRedirection();
+//Plant EndPoints
+app.MapGet("api/LabManager/plant/", async (LabManagerDBContext context) =>
+{
+	var plants = await context.Plants.Include(p => p.Protocols).ToListAsync();
+	return Results.Ok(plants);
+});
 
+app.MapGet("api/LabManager/plant/{id}", async (LabManagerDBContext context, int id) =>
+{
+	var plants = await context.Plants.Include(p => p.Protocols).FirstOrDefaultAsync(pl => pl.Id == id);
+	return Results.Ok(plants);
+});
+
+
+app.MapPost("api/LabManager/plant/", async (LabManagerDBContext context, IMapper mapper, PlantDTO plantDTO) =>
+{
+	var plant = mapper.Map<Plant>(plantDTO);
+	plant.TotalQt = plantDTO.InTSQt + plantDTO.MotherPlantsQt;
+
+	await context.Plants.AddAsync(plant);
+
+	await context.SaveChangesAsync();
+
+	return Results.Created($"api/LabManager/plant/{plant.Id}", plant);
+
+});
+
+app.MapPut("api/LabManager/plant/{id}/", async (LabManagerDBContext context, IMapper mapper, PlantDTO plantDTO) =>
+{
+	Plant updatedPlant = new Plant();
+
+	updatedPlant = mapper.Map<Plant>(plantDTO);
+	updatedPlant.TotalQt = plantDTO.InTSQt + plantDTO.MotherPlantsQt + plantDTO.ForSaleQt;
+	context.Update(updatedPlant);
+	await context.SaveChangesAsync();
+	return Results.NoContent();
+
+});
+
+app.MapDelete("api/LabManager/plant/{id}", async (LabManagerDBContext context, int id) =>
+{
+	var plantModel = await context.Plants.FirstOrDefaultAsync(pl => pl.Id == id);
+	if (plantModel == null)
+	{
+		return Results.NotFound();
+	}
+	context.Plants.Remove(plantModel);
+	await context.SaveChangesAsync();
+
+	return Results.NoContent();
+});
+
+//Protocol Endpoints
+
+app.MapGet("api/LabManager/protocols", async (LabManagerDBContext context) =>
+{
+	var protocols = await context.Protocols.Include(p => p.Plant)
+		.ToListAsync();
+	return Results.Ok(protocols);
+});
+app.MapGet("api/LabManager/protocols/{id}", async (LabManagerDBContext context, int id) =>
+{
+	var protocols = await context.Protocols.FirstOrDefaultAsync(pl => pl.Id == id);
+	return Results.Ok(protocols);
+});
+
+app.MapPost("api/LabManager/protocols/", async (LabManagerDBContext context, IMapper mapper, ProtocolDTO protocolDTO) =>
+{
+	var protocol = mapper.Map<Protocol>(protocolDTO);
+
+	await context.Protocols.AddAsync(protocol);
+
+	await context.SaveChangesAsync();
+
+	return Results.Created($"api/LabManager/plant/{protocol.Id}", protocol);
+
+});
+
+app.MapPut("api/LabManager/protocols/{id}/", async (LabManagerDBContext context, IMapper mapper, ProtocolDTO protocolDTO) =>
+{
+	var updatedProtocol = mapper.Map<Protocol>(protocolDTO);
+	context.Update(updatedProtocol);
+	await context.SaveChangesAsync();
+	return Results.NoContent();
+
+});
+
+app.MapDelete("api/LabManager/protocols/{id}", async (LabManagerDBContext context, int id) =>
+{
+	var protocolModel = await context.Protocols.FirstOrDefaultAsync(pr => pr.Id == id);
+	if (protocolModel == null)
+	{
+		return Results.NotFound();
+	}
+	context.Protocols.Remove(protocolModel);
+	await context.SaveChangesAsync();
+	return Results.NoContent();
+});
+
+//Media endpoint
+
+app.MapPost("api/LabManager/media/", async (LabManagerDBContext context, IMapper mapper, MediaDTO mediaDTO) =>
+{
+	var media = mapper.Map<Media>(mediaDTO);
+	await context.Media.AddAsync(media);
+
+	await context.SaveChangesAsync();
+
+	return Results.Created($"api/LabManager/plant/{media.Id}", media);
+
+});
+app.MapGet("api/LabManager/media", async (LabManagerDBContext context) =>
+{
+	var media = await context.Media.Include(p => p.Protocol)
+		.ToListAsync();
+	return Results.Ok(media);
+});
+
+//Ingredient endpoint
+app.MapGet("api/LabManager/ingredient", async (LabManagerDBContext context) =>
+{
+	var ingredients = await context.Ingredients.ToListAsync();
+	return Results.Ok(ingredients);
+});
+
+app.MapGet("api/LabManager/ingredient/{name}", async (LabManagerDBContext context, string name) =>
+{
+	var ingredient = await context.Ingredients.FirstOrDefaultAsync(pl => pl.Name == name);
+	return Results.Ok(ingredient);
+});
+
+app.MapPost("api/LabManager/ingredient", async (LabManagerDBContext context, Ingredient ingredient) =>
+{
+	await context.Ingredients.AddAsync(ingredient);
+
+	await context.SaveChangesAsync();
+
+	return Results.Created($"api/LabManager/ingredient/{ingredient.Name}", ingredient);
+
+});
+
+app.MapPut("api/LabManager/ingredient/{name}", async (LabManagerDBContext context, Ingredient ingredient) =>
+{
+	var ingredientModel = await context.Ingredients.FirstOrDefaultAsync(pr => pr.Name == ingredient.Name);
+	if (ingredientModel == null)
+	{
+		return Results.NotFound();
+	}
+	ingredientModel.Quantity = ingredient.Quantity;
+	if (ingredient.ListOfMedias != null)
+	{
+		foreach (var ingedient in ingredient.ListOfMedias)
+		{
+			context.Add(ingedient);
+		}
+		context.SaveChanges();
+	}
+
+	await context.SaveChangesAsync();
+
+	return Results.NoContent();
+
+});
+
+app.MapDelete("api/LabManager/ingredient/{id}", async (LabManagerDBContext context, int id) =>
+{
+	var ingredient = await context.Ingredients.FirstOrDefaultAsync(pr => pr.Id == id);
+	if (ingredient == null)
+	{
+		return Results.NotFound();
+	}
+	context.Ingredients.Remove(ingredient);
+	await context.SaveChangesAsync();
+	return Results.NoContent();
+});
+//
 app.Run();
 
