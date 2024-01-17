4e2c75b53364d6a30d2cbe288980aed919331dec
Migle Urbonaite <91160873+MistyDoe@users.noreply.github.com>
2024-01-17 21:29:10 +0100

    API: set up controller for Plant as a non-mimimal API controller

---
+++ API/Controllers/PlantsController.cs
@@ -0,0 +1,96 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+[Route("api/LabManager/[controller]")]
+[ApiController]
+public class PlantsController : ControllerBase
+{
+	private readonly LabManagerDBContext _context;
+	private readonly IMapper _mapper;
+
+	public PlantsController(LabManagerDBContext context, IMapper mapper)
+	{
+		_context = context;
+		_mapper = mapper;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> Index()
+	{
+		return _context.Plants != null ? Ok(
+					await _context.Plants
+					.Include(p => p.Protocols)
+					.ToListAsync()) : NotFound();
+
+	}
+
+	[HttpGet]
+	[Route("api/LabManager/[controller]/{id}")]
+	public async Task<IActionResult> Details(int? id)
+	{
+		if (id == null || _context.Plants == null)
+		{
+			return NotFound();
+		}
+
+		var plant = await _context.Plants
+			.Include(p => p.Protocols)
+			.FirstOrDefaultAsync(pl => pl.Id == id);
+		if (plant == null)
+		{
+			return NotFound();
+		}
+
+		return Ok(plant);
+	}
+
+
+
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> Create(PlantDTO plantDTO)
+	{
+		var plant = _mapper.Map<Plant>(plantDTO);
+		plant.TotalQt = plantDTO.InTSQt + plantDTO.MotherPlantsQt;
+
+		await _context.Plants.AddAsync(plant);
+
+		await _context.SaveChangesAsync();
+
+		return Ok(plant);
+
+	}
+
+	// GET: Plants/Edit/5
+	[HttpPatch]
+	public async Task<IActionResult> Edit(PlantDTO plantDTO)
+	{
+		Plant updatedPlant = new Plant();
+
+		updatedPlant = _mapper.Map<Plant>(plantDTO);
+		updatedPlant.TotalQt = plantDTO.InTSQt + plantDTO.MotherPlantsQt + plantDTO.ForSaleQt;
+		_context.Update(updatedPlant);
+		await _context.SaveChangesAsync();
+		return Ok();
+	}
+
+	// GET: Plants/Delete/5
+	[HttpDelete]
+	public async Task<IActionResult> Delete(int? id)
+	{
+		var plantModel = await _context.Plants.FirstOrDefaultAsync(pl => pl.Id == id);
+		if (plantModel == null)
+		{
+			return BadRequest("Plant not found");
+		}
+		_context.Plants.Remove(plantModel);
+		await _context.SaveChangesAsync();
+		return Ok();
+	}
+
+}
---
+++ API/Controllers/ProtocolsController.cs
@@ -0,0 +1,102 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+	[Route("api/LabManager/[controller]")]
+	[ApiController]
+	public class ProtocolsController : ControllerBase
+	{
+		private readonly LabManagerDBContext _context;
+		private readonly IMapper _mapper;
+
+		public ProtocolsController(LabManagerDBContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+
+		// GET: api/Protocols
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Protocol>>> GetProtocols()
+		{
+			var protocols = await _context.Protocols.Include(p => p.Plant)
+		  .ToListAsync();
+			return Ok(protocols);
+		}
+
+		// GET: api/Protocols/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Protocol>> GetProtocol(int id)
+		{
+			if (_context.Protocols == null)
+			{
+				return NotFound();
+			}
+			var protocol = await _context.Protocols.FirstOrDefaultAsync(pr => pr.Id == id);
+
+			if (protocol == null)
+			{
+				return NotFound();
+			}
+
+			return protocol;
+		}
+
+		// PUT: api/Protocols/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutProtocol(int id, ProtocolDTO protocolDTO)
+		{
+			var updatedProtocol = _mapper.Map<Protocol>(protocolDTO);
+			_context.Update(updatedProtocol);
+			await _context.SaveChangesAsync();
+			return Ok();
+
+		}
+
+		// POST: api/Protocols
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<Protocol>> PostProtocol(Protocol protocol)
+		{
+			if (_context.Protocols == null)
+			{
+				return Problem("Entity set 'LabManagerDBContext.Protocols'  is null.");
+			}
+			_context.Protocols.Add(protocol);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("GetProtocol", new { id = protocol.Id }, protocol);
+		}
+
+		// DELETE: api/Protocols/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteProtocol(int id)
+		{
+			if (_context.Protocols == null)
+			{
+				return NotFound();
+			}
+			var protocol = await _context.Protocols.FindAsync(id);
+			if (protocol == null)
+			{
+				return NotFound();
+			}
+
+			_context.Protocols.Remove(protocol);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		private bool ProtocolExists(int id)
+		{
+			return (_context.Protocols?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
+}
